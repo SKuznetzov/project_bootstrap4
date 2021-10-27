35f9b1b729e17799d5aa8ff7ac7fe83258d8c41b
SKuznetzov <s-kuznecov100@rambler.ru>
2021-10-27 13:33:38 +0300

    add js function

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+        $('.button[filter]').click(function() {
+        if ($(this).attr('val') == 'off') {
+            $('.button[filter]').attr('val', 'off').removeClass('focused');
+            $(this).attr('val', 'on').addClass('focused');
+            $('.filter > div').hide(300);
+            $('.filter > div[filter=' + $(this).attr('filter') + ']').show(300);
+            if ($(this).attr('filter') == 'all') {
+                $('.button[filter]').attr('val', 'off').removeClass('focused');
+                $(this).attr('val', 'on').addClass('focused');
+                $('.filter > div').show(300);
+            }
+        }
+    });
+    $('.multiple-items').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: false,
+        dots: true,
+        dotsClass: 'dots-style'
+    });
+});
